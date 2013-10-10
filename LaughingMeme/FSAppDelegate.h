e18df2a07cddff8dacfd975348c7522a18530788
Dominik Kapusta <dominik@getbase.com>
2013-10-10 19:16:58 +0200

    Additional Initial commit

---
+++ LaughingMeme/FSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FSAppDelegate.h
+//  LaughingMeme
+//
+//  Created by Dominik Kapusta on 10.10.2013.
+//  Copyright (c) 2013 Future Simple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LaughingMeme/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LaughingMeme
+//
+//  Created by Dominik Kapusta on 10.10.2013.
+//  Copyright (c) 2013 Future Simple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FSAppDelegate class]));
+    }
+}
